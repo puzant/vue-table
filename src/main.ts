82faf4bfcfdd1d3a4ae6e4e20a26b1da8b4b7388
Puzant <PBAKJEJIAN@hotmail.com>
2020-01-12 20:10:42 +0200

    use loadash as gobal dependancy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import _ from 'lodash'
+
 
 Vue.config.productionTip = false
+
+Vue.prototype._ = _
 
 new Vue({
   store,
