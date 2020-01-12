4bf5bd60976dba37d6cbfaf13c3055fa2392a291
Puzant <PBAKJEJIAN@hotmail.com>
2020-01-12 20:10:55 +0200

    add new data props

---
+++ src/table_data.js
@@ -0,0 +1,124 @@
+export default {
+  "data": [
+    {
+      "id": "0",
+      "creators_details": {
+        "talent_avatar": "talent-",
+        "talent_name": "Xing Chang",
+        "number_of_posts": "3"
+      },
+      "impressions": null,
+      "engagement": {
+        "value": "120",
+        "percentage": "30"
+      },
+      "video_views": {
+        "number_of_views": null,
+        "view_percentage": null
+      },
+      "cost": "120",
+      "cpv": "43",
+      "cpe": "65"
+     },
+     {
+      "id": "1",
+      "creators_details": {
+        "talent_avatar": "talent-",
+        "talent_name": "James AuKland",
+        "number_of_posts": "5"
+      },
+      "impressions": "1500",
+      "engagement": {
+        "value": "40",
+        "percentage": "30"
+      },
+      "video_views": {
+        "number_of_views": "1000",
+        "view_percentage": "30%"
+      },
+      "cost": "2000",
+      "cpv": "1000",
+      "cpe": "4000"
+     },
+     {
+      "id": "2",
+      "creators_details": {
+        "talent_avatar": "talent-",
+        "talent_name": "Mary Svetlan",
+        "number_of_posts": "2"
+      },
+      "impressions": "2730",
+      "engagement": {
+        "value": "87",
+        "percentage": "30"
+      },
+      "video_views": {
+        "number_of_views": "1000",
+        "view_percentage": "30%"
+      },
+      "cost": "2000",
+      "cpv": "1000",
+      "cpe": "4000"
+     },
+     {
+      "id": "3",
+      "creators_details": {
+        "talent_avatar": "talent-",
+        "talent_name": "Jonthan Hail",
+        "number_of_posts": "1"
+      },
+      "impressions": "2000",
+      "engagement": {
+        "value": "90",
+        "percentage": "45"
+      },
+      "video_views": {
+        "number_of_views": "1000",
+        "view_percentage": "30%"
+      },
+      "cost": "2000",
+      "cpv": "1000",
+      "cpe": "4000"
+     },
+     {
+      "id": "4",
+      "creators_details": {
+        "talent_avatar": "talent-",
+        "talent_name": "Amanda soulop",
+        "number_of_posts": "12"
+      },
+      "impressions": "2300",
+      "engagement": {
+        "value": "150",
+        "percentage": "30"
+      },
+      "video_views": {
+        "number_of_views": "1000",
+        "view_percentage": "10%"
+      },
+      "cost": "2000",
+      "cpv": "1000",
+      "cpe": "4000"
+     },
+     {
+      "id": "5",
+      "creators_details": {
+        "talent_avatar": "talent-",
+        "talent_name": "Samantha strobe",
+        "number_of_posts": "1"
+      },
+      "impressions": "100",
+      "engagement": {
+        "value": "200",
+        "percentage": "30"
+      },
+      "video_views": {
+        "number_of_views": "1000",
+        "view_percentage": "90%"
+      },
+      "cost": "2000",
+      "cpv": "1000",
+      "cpe": "4000"
+     }
+  ]
+}
